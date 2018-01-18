18a0f594c698499bc123970453b7bb14238c43ab
Priyadharshan Ravi <32198696+PriyadharshanRavi@users.noreply.github.com>
2018-01-18 15:37:34 +0530

    Add files via upload

---
+++ Basic01/src/Basic.java
@@ -0,0 +1,16 @@
+public class Basic {
+    public static void main(String[] args){
+        System.out.println("i'm back again");
+
+        int myFirstNumber=11;
+        int mySecondNumber=1;
+        int myThirdNumber=100;
+
+        int myTotal=myFirstNumber+mySecondNumber+myThirdNumber;
+        int myLastOne=1000-myTotal;
+        System.out.println("That is my favourite number " + myFirstNumber);
+        System.out.println("My total is " + myTotal);
+        System.out.println("task: " + myLastOne);
+
+    }
+}
