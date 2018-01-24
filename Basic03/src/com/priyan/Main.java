c9553052e966c515168b3a6f72c1334d07ad56e7
Priyadharshan Ravi <32198696+PriyadharshanRavi@users.noreply.github.com>
2018-01-25 01:49:17 +0530

    Add files via upload

---
+++ Basic03/src/com/priyan/Main.java
@@ -0,0 +1,13 @@
+package com.priyan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myPoundValue = 20;
+	    double myKiloValue = 0.45350237;
+	    System.out.println("Weight in pounds = " + myPoundValue);
+        System.out.println("Converting pounds into kilos....");
+        System.out.println("Result = " +((myPoundValue*myKiloValue)) + " Kilos");
+
+    }
+}
