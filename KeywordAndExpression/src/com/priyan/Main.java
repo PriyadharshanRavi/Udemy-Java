93d620aca0914989f3b3cce9938dbbaac9ff6adb
Priyadharshan Ravi <32198696+PriyadharshanRavi@users.noreply.github.com>
2018-01-27 02:09:09 +0530

    Add files via upload

---
+++ KeywordAndExpression/src/com/priyan/Main.java
@@ -0,0 +1,27 @@
+package com.priyan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //One kinda expression
+        //A mile is equal to 1.609344 kilometres
+        //there are 53 reserved keywords in java like int, float, double, false, true, etc,.
+        double kilometres = (100 * 1.609344);
+
+        //another kinda expression
+        int highscore = 100;
+        if(highscore == 100){
+            System.out.println("This is an expression");
+        }
+
+        int score = 100; //score=100 is an expression
+        if(score > 90){ //score > 90 is an expression
+            System.out.println("You got a high score"); //you got high score is an expression
+            score = 0; //score  = 0 is an expression
+
+        }
+
+
+
+    }
+}
