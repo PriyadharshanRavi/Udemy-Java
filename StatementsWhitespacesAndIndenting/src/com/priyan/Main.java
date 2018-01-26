f3ca8ed857a7ad06f96fe8bfaa63f32f70c64a27
Priyadharshan Ravi <32198696+PriyadharshanRavi@users.noreply.github.com>
2018-01-27 03:59:22 +0530

    Add files via upload

---
+++ StatementsWhitespacesAndIndenting/src/com/priyan/Main.java
@@ -0,0 +1,23 @@
+package com.priyan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        System.out.println("this is a test");
+
+        //single expression in many lines
+        System.out.println("this is " +
+        "a " +
+        "test ");
+
+        //many expression on the same line
+        int anotherVaribale = myVariable++;myVariable--;
+        System.out.println("test again: " + anotherVaribale);
+
+
+
+
+
+    }
+}
