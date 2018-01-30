7722792eccb593fb7b66c9f47543aa89d42e0d07
Priyadharshan Ravi <32198696+PriyadharshanRavi@users.noreply.github.com>
2018-01-31 05:01:59 +0530

    Add files via upload

---
+++ Inheritanceex/src/com/priyan/Main.java
@@ -0,0 +1,13 @@
+package com.priyan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Porsche porsche = new Porsche(36);
+        porsche.accelerate(30);
+        porsche.accelerate(20);
+        porsche.accelerate(-42);
+
+    }
+}
